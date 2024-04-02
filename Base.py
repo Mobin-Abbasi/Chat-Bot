b27fd620c7b35788f546d37ae4d27202bf3b6660
Mobin <mobin04dev@gmail.com>
2024-04-03 01:50:55 +0330

    .

--- Base.py
+++ Base.py
@@ -2,7 +2,9 @@ import pyautogui as pg
 from time import sleep
 from random import choice
 
-lst = ['Hello', 'My Name is Mobin', 'But', 'Fuck You', 'I love you']
-pg.click(1111, 963)
-pg.write("dani kiram dahanet", interval=0.1)
-pg.press('enter')
+lst = ['I Love You', 'I Hate You', 'Hi There', "What's up?", "How Are You?"]
+for i in range(5):
+    sleep(1)
+    pg.click(1111, 963)
+    pg.write(choice(lst), interval=0.1)
+    pg.press('enter')
