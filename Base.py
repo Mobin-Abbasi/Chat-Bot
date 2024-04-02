3ecc5b8fc7bee02176c401d3b41d664af0b8456e
Mobin <mobin04dev@gmail.com>
2024-04-03 01:16:20 +0330

    Add list

--- Base.py
+++ Base.py
@@ -2,6 +2,7 @@ import pyautogui as pg
 from time import sleep
 from random import choice
 
+lst = ['Hello', 'My Name is Mobin', 'But', 'Fuck You', 'I love you']
 pg.click(1111, 963)
 pg.write("dani kiram dahanet", interval=0.1)
 pg.press('enter')
