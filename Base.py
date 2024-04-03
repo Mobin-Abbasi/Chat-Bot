ebc8b2dffc793395bd415de85770fa3ce55cad18
Mobin <mobin04dev@gmail.com>
2024-04-03 22:56:38 +0330

    Update bot

--- Base.py
+++ Base.py
@@ -2,9 +2,21 @@ import pyautogui as pg
 from time import sleep
 from random import choice
 
-lst = ['I Love You', 'I Hate You', 'Hi There', "What's up?", "How Are You?"]
-for i in range(5):
-    sleep(1)
-    pg.click(1111, 963)
-    pg.write(choice(lst), interval=0.1)
-    pg.press('enter')
+pg.click(229, 1049)
+pg.write('microsoft edge', interval=0.2)
+sleep(1)
+pg.click(277, 377)
+sleep(1)
+pg.click(749, 61)
+pg.write('telegram', interval=0.2)
+pg.press('enter')
+sleep(4)
+pg.click(331, 487)
+sleep(4)
+pg.click(247, 117)
+pg.write('python', interval=0.2)
+pg.click(239, 357)
+sleep(1)
+pg.click(1111, 963)
+pg.write('Hi There', interval=0.2)
+pg.press('enter')
